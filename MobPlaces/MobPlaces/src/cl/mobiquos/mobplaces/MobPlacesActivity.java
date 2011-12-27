11ef11bedd9f715a637c9cfd79e2949ebba4d0d9
Manuel Alejandro Moscoso Domínguez <mmoscoso@mobiquos.cl>
2011-12-27 12:31:21 -0300

    Add files from Android Project

---
+++ MobPlaces/MobPlaces/src/cl/mobiquos/mobplaces/MobPlacesActivity.java
@@ -0,0 +1,13 @@
+package cl.mobiquos.mobplaces;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MobPlacesActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
