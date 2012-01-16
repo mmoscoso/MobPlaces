5edc6eac7115cea0906f7a22529f375cc7640bf7
Manuel Alejandro Moscoso Domínguez <mmoscoso@mobiquos.cl>
2012-01-16 14:59:37 -0300

    Add class for data event

---
+++ MobPlaces/MobPlaces/src/cl/mobiquos/mobplaces/data/PlaceEvent.java
@@ -0,0 +1,15 @@
+package cl.mobiquos.mobplaces.data;
+
+
+/**
+ * 
+ * @author manuel moscoso dominguez 01-2012
+ * 
+ *
+ */
+public class PlaceEvent {
+	private String Name;
+	private int id;
+	private int user_id;
+	private String description;
+}
